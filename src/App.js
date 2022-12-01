368188864f62b1dff22d73d21d82bc8521afa5b0
unknown <aquinasmoriasi@gmail.com>
2022-12-01 18:22:45 +0300

    add route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import Nav from './components/Nav';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { fetchGreeting } from './redux/greetings/greeting';
+import Greeting from './components/Greeting';
 
-function App() {
-  return <Nav />;
-}
+const App = () => {
+  const dispatch = useDispatch();
+  const greet = () => {
+    dispatch(fetchGreeting());
+  };
+  return (
+    <>
+      <header>
+        <nav>
+          <NavLink to="/Greeting" onClick={greet}>
+            Greetings
+          </NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/Greeting" element={<Greeting />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
